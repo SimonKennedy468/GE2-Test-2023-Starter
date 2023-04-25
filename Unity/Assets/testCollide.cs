27088bced77b1d96fa451eb6ebfc1072907a3ea2
Simon Kennedy <simonkennedy468@gmail.com>
2023-04-25 10:16:00 +0100

    camera enters point and looks at head, but snaps to it rather than gradual turn

--- Unity/Assets/testCollide.cs
+++ Unity/Assets/testCollide.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 
 public class testCollide : MonoBehaviour
 {
+    public GameObject camera;
+    public bool isCollided = false;
+    public GameObject target;
+    public GameObject head;
+    public Vector3 dir;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,11 +17,20 @@ public class testCollide : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isCollided == true)
+        {
+            camera.transform.LookAt(head.transform);
+            dir = target.transform.position - camera.transform.position;
+            camera.transform.Translate(dir);
+        }
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("entered");
+        target = other.gameObject; 
+        //this.transform.position = other.transform.position;
+        isCollided = true;
     }
 }
